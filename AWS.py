036b8dff9e631c0f44b590173c139effd40d5251
Jack Konyan <jackkonyan@gmail.com>
2024-10-08 19:10:04 -0700

    Creation of Database and AWS helper classes

--- AWS.py
+++ AWS.py
@@ -5,13 +5,16 @@ class AWS:
     aws_client = None
     def __init__(self):
         self.aws_client = boto3.resource('dynamodb')
+   
+        
+
+
+class AWS_DB(AWS):
     def get_table(self, table_name):
         response = self.aws_client.Table(table_name)
-        
         return response
-
 if __name__ == '__main__':
-    aws=AWS()
-    table = aws.get_table('586_DB')
+    DB_table = AWS_DB()
+    table = DB_table.get_table('586_DB')
     print(table)
     print(table.item_count)
