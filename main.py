1322c0d38c00bed0ce6ba88114cf53c91076c631
Chance Chime <cchime82@icloud.com>
2024-10-22 03:27:21 -0700

    Updated Backend with Login, Register, Main -- Need Database to continue

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 # Note: Import send_file, qrcode
-from flask import Flask, send_file
+from flask import Flask, render_template
+from AWS import AWS
 
+#Database = AWS()
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
